326e21edad0667f04068573af52f9eec8eb0c420
KristineGray <krisgraya@gmail.com>
2021-06-28 15:06:03 -0400

    added fields to menu item class

--- RestaurantMenu/RestaurantMenu/MenuItem.cs
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -8,5 +8,9 @@ namespace RestaurantMenu
 {
     class MenuItem
     {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew;
     }
 }
