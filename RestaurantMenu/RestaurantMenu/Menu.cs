1b99f5cd181af5b42d6affcdf5016a1d3910cd19
Carter McKaughan <cartermckaughan@Carters-MacBook-Pro.local>
2021-06-28 12:26:24 -0700

    Added items to menu

--- RestaurantMenu/RestaurantMenu/Menu.cs
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -8,5 +8,25 @@ namespace RestaurantMenu
 {
     class Menu
     {
+        public Menu()
+        {
+            MenuItem FriedRice = new MenuItem();
+            FriedRice.Price = 8.99;
+            FriedRice.Description = ("White jasmine rice with shrimp, egg, and green onions.");
+            FriedRice.Category = ("Main Course");
+            FriedRice.IsNew = true;
+
+            MenuItem WonTon = new MenuItem();
+            WonTon.Price = 3.95;
+            WonTon.Description = ("Small piece of meat encased in dough, with spicy sauce. Order of 6.");
+            WonTon.Category = ("Appetizer");
+            WonTon.IsNew = true;
+
+            MenuItem FriedIceCream = new MenuItem();
+            FriedIceCream.Price = 2.50;
+            FriedIceCream.Description = ("One scoop of vanilla icecream deep fried.");
+            FriedIceCream.Category = ("Dessert");
+            FriedIceCream.IsNew = true;
+        }
     }
 }
